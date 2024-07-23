ff3645053636b6422780db90932e3d64b0415b6d
Luccci <lucijan.pavin@gmail.com>
2024-07-23 19:43:32 +0200

    REEEEEE

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,24 @@
 import { signOut } from 'firebase/auth'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Alert } from 'react-bootstrap'
 import { auth } from '../../firebase'
+import { onAuthStateChanged } from 'firebase/auth'
+import { IoIosClose } from "react-icons/io";
 
 function Navbar() {
     const [showAlert, setShowAlert] = useState(false)
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        // Listener for authentication state changes
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser); // Set user based on sign-in status
+        });
+        return unsubscribe; // Cleanup subscription on unmount
+    }, [])
+
     const onClickHandler = () => {
         setShowAlert(!showAlert)
     }
@@ -28,17 +40,38 @@ function Navbar() {
         <nav>
             <ul>
                 <li><Link to="home">Lame A$$ Chat</Link></li>
-                <li><Link to="signin">Sign In</Link></li>
-                <li><button type='button' onClick={onClickHandler}>Sign out</button></li>
+                {user ? (
+                        <li className='noPadding'>
+                            <button 
+                                type='button' 
+                                onClick={onClickHandler}
+                                className='signoutbtn'
+                            >
+                                Sign out
+                            </button>
+                        </li>
+                ) : (
+                    <li><Link to="signin">Sign In</Link></li>
+                )}
             </ul>
             <Alert 
                 variant='warning'
-                dismissible
                 onClose={onClickHandler}
                 show={showAlert}
+                id='alert'
             >
+                <button onClick={onClickHandler} id='closebtn'><IoIosClose /></button>
                 Are you sure you want to sign out?
-                <button onClick={() => {onClickHandler(); handleSignOut()}}>Sign out</button>
+                <button 
+                    onClick={() => 
+                    {
+                        onClickHandler(); 
+                        handleSignOut()
+                    }}
+                    className='signoutbtn'
+                >
+                    Sign out
+                </button>
             </Alert>
         </nav>
     )
