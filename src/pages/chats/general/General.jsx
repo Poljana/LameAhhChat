435f76e8d52102b10f0df11a814ff7f056e1ba01
Luccci <lucijan.pavin@gmail.com>
2024-07-30 22:45:08 +0200

    covice drugi puta

--- src/pages/chats/general/General.jsx
+++ src/pages/chats/general/General.jsx
@@ -1,28 +1,41 @@
 import './General.css'
 import { useEffect, useState } from 'react';
-import { doc, addDoc, collection, deleteDoc, getDocs, Timestamp } from 'firebase/firestore';
-import { db } from '../../../firebase';
+import { doc, addDoc, collection,
+         deleteDoc, getDocs, Timestamp,
+          query, orderBy } from 'firebase/firestore';
+import { auth, db } from '../../../firebase';
 import { IoMdSend, IoIosTrash } from "react-icons/io";
+import { onAuthStateChanged } from 'firebase/auth';
 
 
 const General = ({ outerCollectionName, outerDocId, nestedCollectionName }) => {
     const [nestedDocuments, setNestedDocuments] = useState([]);
     const [message, setMessage] = useState('')
+    const [currentUserId, setCurrentUserId] = useState('')
+
+    const fetchNestedCollection = async () => {
+        try {
+            const nestedCollectionRef = collection(db, `${outerCollectionName}/${outerDocId}/${nestedCollectionName}`);
+            const q = query(nestedCollectionRef, orderBy('time', 'asc'))
+            const querySnapshot = await getDocs(q);
+            const docsData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            setNestedDocuments(docsData);
+        } catch (error) {
+            console.error("Error fetching nested collection: ", error);
+        }
+    };
 
     useEffect(() => {
-        const fetchNestedCollection = async () => {
-            try {
-                const nestedCollectionRef = collection(db, `${outerCollectionName}/${outerDocId}/${nestedCollectionName}`);
-                const querySnapshot = await getDocs(nestedCollectionRef);
-                const docsData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-                setNestedDocuments(docsData);
-            } catch (error) {
-                console.error("Error fetching nested collection: ", error);
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setCurrentUserId(user.uid)
+            } else {
+                setCurrentUserId('')
             }
-        };
+        })
 
-        fetchNestedCollection();
-    }, [outerCollectionName, outerDocId, nestedCollectionName]);
+        return () => unsubscribe()
+    }, [])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -32,13 +45,11 @@ const General = ({ outerCollectionName, outerDocId, nestedCollectionName }) => {
             const nestedCollectionRef = collection(db, `${outerCollectionName}/${outerDocId}/${nestedCollectionName}`)
             await addDoc(nestedCollectionRef, {
                 content : message,
-                time : Timestamp.now()
+                time : Timestamp.now(),
+                sentBy : currentUserId
             })
             setMessage('')
-
-            const querySnapshot = await getDocs(nestedCollectionRef)
-            const docsData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data()}))
-            setNestedDocuments(docsData)
+            fetchNestedCollection()
         } catch (error) {
             console.error("Error adding document: ", error)
         }
@@ -48,26 +59,62 @@ const General = ({ outerCollectionName, outerDocId, nestedCollectionName }) => {
         try {
             const docRef = doc(db, `${outerCollectionName}/${outerDocId}/${nestedCollectionName}`, id)
             await deleteDoc(docRef)
-
-            setNestedDocuments(prevDocs => prevDocs.filter(doc => doc.id !== id))
+            fetchNestedCollection()
         } catch (error) {
             console.error("Error deleting document: ", error)
         }
     }
+    
+    useEffect(() => {
+        fetchNestedCollection();
+    }, []);
+
+    const groupMessages = (messages) => {
+        const groupedMessages = [];
+        let currentGroup = [];
+
+        messages.forEach((message, index) => {
+            if (index === 0 || message.sentBy === messages[index - 1].sentBy) {
+                currentGroup.push(message);
+            } else {
+                groupedMessages.push(currentGroup);
+                currentGroup = [message];
+            }
+        });
+
+        if (currentGroup.length > 0) {
+            groupedMessages.push(currentGroup);
+        }
+
+        return groupedMessages;
+    };
+
+    const groupedMessages = groupMessages(nestedDocuments);
+
+    const MessageGroup = ({ messages, currentUserId }) => {
+        return (
+            <div className={`messages ${messages[0].sentBy === currentUserId ? 'message-right' : 'message-left'}`}>
+                {messages.map(doc => (
+                    <div key={doc.id} className="message">
+                        <p className="message-content">
+                            {doc.content}
+                        </p>
+                        {doc.sentBy === currentUserId && (
+                            <button onClick={() => handleDelete(doc.id)} className='deleteButton'>
+                                <IoIosTrash />
+                            </button>
+                        )}
+                    </div>
+                ))}
+            </div>
+        );
+    };
 
     return (
         <div className='generalChat'>
             <div className='chatField'>
-                {nestedDocuments.map(doc => (
-                    <div key={doc.id} className='messages'>
-                        <p>{doc.content}</p>
-                        <button 
-                            onClick={() => handleDelete(doc.id)}
-                            className='deleteButton'
-                        >
-                                <IoIosTrash />
-                        </button>
-                    </div>
+                {groupedMessages.map((group, index) => (
+                    <MessageGroup key={index} messages={group} currentUserId={currentUserId} />
                 ))}
             </div>
             <form className='messageArea' onSubmit={handleSubmit}>
