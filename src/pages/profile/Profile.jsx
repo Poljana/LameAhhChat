df6a3e590df27ecea00b185e2ba96a6f2607c304
Luccci <lucijan.pavin@gmail.com>
2024-08-06 21:03:04 +0200

    Triba dovrsiti

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from 'react'
 import './Profile.css'
-import { doc, getDoc } from 'firebase/firestore'
+import { doc, getDoc, updateDoc } from 'firebase/firestore'
 import { db } from '../../firebase'
 
 function Profile({ userId }) {
     const [userData, setUserData] = useState('')
+    const [editUsername, setEditUsername] = useState(false)
+    const [newUsername, setNewUsername] = useState('')
+    const [editEmail, setEditEmail] = useState(false)
+    const [newEmail, setNewEmail] = useState('')
 
     useEffect(() => {
         const fetchUserData = async () => {
@@ -27,10 +31,74 @@ function Profile({ userId }) {
         }
     }, [userId])
 
+    const handleUsername = () => {
+        setEditUsername(!editUsername)
+    }
+
+    const handleUsernameEdit = async (e) => {
+        e.preventDefault()
+        
+        try {
+            const docRef = doc(db, "users", userId)
+            await updateDoc(docRef, {
+                username: newUsername
+            })
+            setUserData(prevData => ({ ...prevData, username: newUsername }))
+            setEditUsername(false)
+        } catch (error) {
+            console.error("Error updating document: ", error)
+        }
+    }
+
+    const handleEmailEdit = async (e) => {
+        
+    }
+
     return (
         <div id='userProfile'>
-            <p>Personal info</p>
-
+            <h3>Personal info</h3>
+            <div className='username'>
+                {
+                    editUsername ?
+                    <form className='editForm' onSubmit={handleUsernameEdit}>
+                        <input 
+                            type='text' 
+                            placeholder='New username...'
+                            value={newUsername}
+                            onChange={(e) => setNewUsername(e.target.value)}    
+                        />
+                        <button type='submit' className='editbtn'>Confirm username</button>
+                    </form> :
+                    <p>Username: {userData.username}</p>
+                }
+                    <button 
+                        className='editbtn' 
+                        onClick={handleUsername}
+                    >
+                        {editUsername ? "Cancel" : "Edit"}
+                    </button>                
+            </div>
+            <div className='email'>
+                {
+                    editEmail ?
+                    <form className='editForm' onSubmit={handleEmailEdit}>
+                        <input 
+                            type='text' 
+                            placeholder='New email...'
+                            value={newEmail}
+                            onChange={(e) => setNewEmail(e.target.value)}    
+                        />
+                        <button type='submit' className='editbtn'>Confirm email</button>
+                    </form> :
+                    <p>Email: {userData.email}</p>
+                }
+                    <button 
+                        className='editbtn' 
+                        onClick={handleEmail}
+                    >
+                        {editEmail ? "Cancel" : "Edit"}
+                    </button>                
+            </div>
         </div>
     )
 }
